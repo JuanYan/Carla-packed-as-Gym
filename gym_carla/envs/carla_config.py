e6c6ac0937ffec64be3615f9ef0bb3974f72e532
Juan Yan <Yan.Juan@warwick.ac.uk>
2018-07-19 13:50:20 +0100

    config update

--- gym_carla/envs/carla_config.py
+++ gym_carla/envs/carla_config.py
@@ -1,11 +1,12 @@
-# CARLA_HOST_ADDRESS = "192.168.1.98" # Carla host address
+# Carla host address
+# CARLA_HOST_ADDRESS = "192.168.1.98" 
+
 # Carla on local machine, use 'localhost'
 CARLA_HOST_ADDRESS = "localhost"
-CARLA_HOST_PORT = 2000  # Port number
+
+# Port number
+CARLA_HOST_PORT = 2000  
 
 # CARLA image shape after resize
 CARLA_IMG_HEIGHT = 60
 CARLA_IMG_WIDTH = 80
-
-# the target location point 134 on the map
-TARGET = [158.08, 27.18]  
